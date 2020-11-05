9694e04f4b90965a2bd3a829ed27237e8ebcc92f
Sahil Sharma <sahil.sharma237333@gmail.com>
2020-11-05 23:52:11 +0000

    exported function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,21 @@
 const request = require('request');
 const url = "https://api.thecatapi.com/v1/breeds/search?q=";
-const name = process.argv.slice(2);
 
-request(url + name, (error, response, body) => {
-  
-  console.log('status code:', response && response.statusCode);
-  if (error) { 
-    console.log(error);
-  } else {
-    const data = JSON.parse(body);
-    if (data.length === 0) {
-      console.log("breed not found");
+const fetchBreedDescription = function(breedName, callback) {
+  request(url + breedName, (error, response, body) => {
+   
+    if (error) { 
+      console.log(error);
     } else {
-      console.log(data[0]['description']);
+      const data = JSON.parse(body);
+      if (data.length === 0) {
+        console.log("breed not found");
+      } else {
+        
+        return callback(null,data[0]['description']);
+      }
     }
-  }
-});
+  });
+};
+
+module.exports = {fetchBreedDescription};
